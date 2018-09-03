9caf861bc9f74c77917aa7082c8c8bca22d2c19e
Kai Wedekind <wedekind.kai@googlemail.com>
2018-09-03 16:06:30 +0200

    Remove zombie code

--- js/color-contrast.js
+++ js/color-contrast.js
@@ -6,31 +6,6 @@
 
   colorCode.value = colorPicker.value.substr(1);
 
-  var rgb2hex = function rgb2hex (red, green, blue) {
-    var rgb = blue | (green << 8) | (red << 16);
-    return '#' + (0x1000000 + rgb).toString(16).slice(1);
-  }
-
-  /*
-  var hex2rgb = function hex2rgb (hex) {
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? {
-      r: parseInt(result[1], 16),
-      g: parseInt(result[2], 16),
-      b: parseInt(result[3], 16)
-    } : null;
-  }
-  */
-
-  var hex2rgb = function hex2rgb (hex) {
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-    return result ? {
-      r: Math.round(parseInt(result[1], 16) / 2.55) / 100,
-      g: Math.round(parseInt(result[2], 16) / 2.55) / 100,
-      b: Math.round(parseInt(result[3], 16) / 2.55) / 100
-    } : null;
-  }
-
   var getContrast50 = function getContrast50(hex) {
     return (parseInt(hex, 16) > 0xffffff/2) ? 'black': 'white';
   }
