ccc08d25e942618156d5c517acbb7d1d60419070
PJ Walker <497242+PJWalker@users.noreply.github.com>
2018-09-07 22:41:41 +1000

    Update color-contrast.js

--- js/color-contrast.js
+++ js/color-contrast.js
@@ -6,7 +6,7 @@
 
   colorCode.value = colorPicker.value.substr(1);
   
-  const toRGB = hex =>   
+  const toRGB = hex => {
     const r = parseInt(hex.substr(1, 2), 16),
           g = parseInt(hex.substr(3, 2), 16),
           b = parseInt(hex.substr(5, 2), 16),
